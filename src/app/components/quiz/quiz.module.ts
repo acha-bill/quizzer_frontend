bd926358b50ed4b0257cb123653e8aded1dcfbe5
Banki Louis <banki@keeeb.com>
2020-08-31 01:33:42 +0100

    feat: added game screen and fixed responsiveness

---
+++ src/app/components/quiz/quiz.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { QuizComponent } from './quiz.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: QuizComponent
+  }
+];
+
+@NgModule({
+  declarations: [QuizComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ]
+})
+
+export class QuizModule { }
